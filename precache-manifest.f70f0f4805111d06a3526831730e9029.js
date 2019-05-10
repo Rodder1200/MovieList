675ae4bb0a2c00e3a5cc5c8e7804790170282108
Roman Goncharuk <roman.goncharuk.dmytrovych@gmail.com>
2019-05-10 19:19:36 +0300

    Updates

---
+++ precache-manifest.f70f0f4805111d06a3526831730e9029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5c0ff9292d165ca6cdbe28861f6429",
+    "url": "/MovieList/index.html"
+  },
+  {
+    "revision": "ad2882b731c3d7a25fe4",
+    "url": "/MovieList/static/css/main.8f384974.chunk.css"
+  },
+  {
+    "revision": "67953307e50f02799632",
+    "url": "/MovieList/static/js/2.55ce8c9f.chunk.js"
+  },
+  {
+    "revision": "ad2882b731c3d7a25fe4",
+    "url": "/MovieList/static/js/main.dca3e957.chunk.js"
+  },
+  {
+    "revision": "3d6ad88b6f7905c89428",
+    "url": "/MovieList/static/js/runtime~main.3318021b.js"
+  },
+  {
+    "revision": "262e3a9f491af6c30ed91e4263bd19b8",
+    "url": "/MovieList/static/media/preloader.262e3a9f.gif"
+  }
+]);
